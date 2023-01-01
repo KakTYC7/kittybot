f12396bcc1a6c47fa0fc82da19d9d1c6f4736738
Нафиков Тимур <nafikovtt@gmail.com>
2023-01-01 09:04:25 +0500

    intercept error

--- kittybot.py
+++ kittybot.py
@@ -18,7 +18,14 @@ URL = 'https://api.thecatapi.com/v1/images/search'
 
 
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)      
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
